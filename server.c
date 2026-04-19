86d75e90a31de72fcb71c7efcf3fe703b0f1619e
Rishi Ramani <rishiramani1512@gmail.com>
2026-04-19 12:30:29 +0000

    Added concurrency allowing multiple clients connection

--- server.c
+++ server.c
@@ -45,109 +45,118 @@ int main(void){
   printf("Server listening on PORT %d.\n",PORT);
   while(1){
     //Accept connection from client
-  client_socket = accept(server_fd,(struct sockaddr*) &address, (socklen_t *)&addrlen); //pointer for addrlen as accept can modify it also struct sockadrr only as we want to define ip4 struct as generic
-  
-  if(client_socket<0){
-    perror("Accept");
-    exit(EXIT_FAILURE);
-  }
-
-  printf("Client Connected!!\n");
-
-  char buffer[2048];
-  //Read client's message
-  int bytes_read = read(client_socket,buffer,sizeof(buffer));
-
-  if(bytes_read<0){
-    perror("Read");
-    exit(EXIT_FAILURE);
-  }
-  else if(bytes_read==0){ //incase client closes connection immediately
-    close(client_socket);
-    continue;
-  }
-
-  buffer[bytes_read] = '\0';
-  printf("%s\n",buffer);
-
-  char* http = strtok(buffer,"\r\n");// get the METHOD /PATH HTTPVERSION line
-  char* method=NULL; //intialization safety
-  char* path=NULL;
-
-  if(http!=NULL){
-    method = strtok(http," ");//split at space
-    path = strtok(NULL," ");//split at space again , NULL = CONTINUE
-    printf("Method : %s\n",method);
-    printf("Path : %s\n",path);
-  }
-  
-  //Respond with HTTP [STATUS] [HEADER] [BODY]
-  char response[16384];
-
-  if(path!=NULL && strcmp(path,"/")==0){
-    //Open html file and serve it
-    FILE *file = fopen("index.html","r");
-    if(file==NULL){
-      perror("File open failed");
-      exit(EXIT_FAILURE);
+    client_socket = accept(server_fd,(struct sockaddr*) &address, (socklen_t *)&addrlen); //pointer for addrlen as accept can modify it also struct sockadrr only as we want to define ip4 struct as generic
+    //Concurrency via fork 
+    int pid = fork();
+    if(pid>0){
+      close(client_socket);
+      continue;
     }
-
-    char file_buffer[8192];
-    int bytes = fread(file_buffer,1,sizeof(file_buffer),file);
-    file_buffer[bytes] = '\0';
-    fclose(file);
-    //return HTML page
-    sprintf(response,
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-    "\r\n"
-    "%s"
-    ,file_buffer);
-  }
-  else{
-    char* filename = path+1; //skip the / and get the filename
-    char* ext = strrchr(filename,'.'); //find extension by finding . last occurence
-    char * content_type;
-    if(ext!=NULL){
-      if(strcmp(ext,".html")==0){
-        content_type="text/html";
-      }else if(strcmp(ext,".css")==0){
-        content_type="text/css";
-      }else if(strcmp(ext,".js")==0){
-        content_type="application/javascript";
-      }else{
-        content_type= "text/plain";
+    else if(pid==0){
+      if(client_socket<0){
+        perror("Accept");
+        exit(EXIT_FAILURE);
       }
-    }else{
-      content_type = "text/plain";
-    }
-    FILE *file = fopen(filename,"r");
-    if(file==NULL){
-      //filename not in server so return page not found
-      sprintf(response,
-      "HTTP/1.1 404 Not Found\r\n"
-      "Content-Type: text/plain\r\n"
-      "\r\n"
-      "Page not found");
-    }
-    else{
-      char file_buffer[8192];
-      int bytes = fread(file_buffer,1,sizeof(file_buffer),file);
-      file_buffer[bytes] = '\0';
-      fclose(file);
-      sprintf(response,
-      "HTTP/1.1 200 OK\r\n"
-      "Content-Type: %s\r\n"
-      "\r\n"
-      "%s"
-      ,content_type, file_buffer);
-    }
-  }
 
-  send(client_socket,response,strlen(response),0);// 0 => flags , no special behaviour
+      printf("Client Connected!!\n");
 
-  //Close sockets
-  close(client_socket);
+      char buffer[2048];
+      //Read client's message
+      int bytes_read = read(client_socket,buffer,sizeof(buffer));
+
+      if(bytes_read<0){
+        perror("Read");
+        exit(EXIT_FAILURE);
+      }
+      else if(bytes_read==0){ //incase client closes connection immediately
+        close(client_socket);
+        continue;
+      }
+
+      buffer[bytes_read] = '\0';
+      printf("%s\n",buffer);
+
+      char* http = strtok(buffer,"\r\n");// get the METHOD /PATH HTTPVERSION line
+      char* method=NULL; //intialization safety
+      char* path=NULL;
+
+      if(http!=NULL){
+        method = strtok(http," ");//split at space
+        path = strtok(NULL," ");//split at space again , NULL = CONTINUE
+        printf("Method : %s\n",method);
+        printf("Path : %s\n",path);
+      }
+      
+      //Respond with HTTP [STATUS] [HEADER] [BODY]
+      char response[16384];
+
+      if(path!=NULL && strcmp(path,"/")==0){
+        //Open html file and serve it
+        FILE *file = fopen("index.html","r");
+        if(file==NULL){
+          perror("File open failed");
+          exit(EXIT_FAILURE);
+        }
+
+        char file_buffer[8192];
+        int bytes = fread(file_buffer,1,sizeof(file_buffer),file);
+        file_buffer[bytes] = '\0';
+        fclose(file);
+        //return HTML page
+        sprintf(response,
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "\r\n"
+        "%s"
+        ,file_buffer);
+      }
+      else{
+        char* filename = path+1; //skip the / and get the filename
+        char* ext = strrchr(filename,'.'); //find extension by finding . last occurence
+        char * content_type;
+        if(ext!=NULL){
+          if(strcmp(ext,".html")==0){
+            content_type="text/html";
+          }else if(strcmp(ext,".css")==0){
+            content_type="text/css";
+          }else if(strcmp(ext,".js")==0){
+            content_type="application/javascript";
+          }else{
+            content_type= "text/plain";
+          }
+        }else{
+          content_type = "text/plain";
+        }
+        FILE *file = fopen(filename,"r");
+        if(file==NULL){
+          //filename not in server so return page not found
+          sprintf(response,
+          "HTTP/1.1 404 Not Found\r\n"
+          "Content-Type: text/plain\r\n"
+          "\r\n"
+          "Page not found");
+        }
+        else{
+          char file_buffer[8192];
+          int bytes = fread(file_buffer,1,sizeof(file_buffer),file);
+          file_buffer[bytes] = '\0';
+          fclose(file);
+          sprintf(response,
+          "HTTP/1.1 200 OK\r\n"
+          "Content-Type: %s\r\n"
+          "\r\n"
+          "%s"
+          ,content_type, file_buffer);
+        }
+      }
+
+      send(client_socket,response,strlen(response),0);// 0 => flags , no special behaviour
+
+      //Close sockets
+      close(client_socket);
+      exit(0);
+    }
+    
   }
   
   close(server_fd);
